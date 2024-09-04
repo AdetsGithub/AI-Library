de44a2846f0149d4e27d28f874271195fdf6cb16
Adethya <adethyasrinivasan@outlook.com>
2024-09-04 17:47:41 +0100

    Added stride to conv layer

--- Layers/convolutional.py
+++ Layers/convolutional.py
@@ -10,11 +10,12 @@ class Convolutional(Layer):
     @param: (int) kernel_size - size of each matrix inside each kernel
     @param: (int) depth - the number of kernels
     """
-    def __init__(self, input_shape, kernel_size, depth):
+    def __init__(self, input_shape, kernel_size, depth, stride=1):
         input_depth, input_height, input_width = input_shape
         self.input_shape = input_shape
         self.input_depth = input_depth
         self.depth = depth
+        self.stride = stride
         self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
         self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
         # Initialise kernels and biases
@@ -32,7 +33,7 @@ class Convolutional(Layer):
         self.input = input
         self.output = np.copy(self.biases)
         for i in range(self.depth):
-            for j in range(self.input_depth):
+            for j in range(0, self.input_depth, self.stride):
                 self.output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], "valid")
         return self.output
 
