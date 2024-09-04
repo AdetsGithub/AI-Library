b12414ebd6fbecb74134a17cca3f4aa90f13aefd
Adethya <adethyasrinivasan@outlook.com>
2024-09-04 20:51:46 +0100

    Implemented SSE

--- Functions/loss_functions.py
+++ Functions/loss_functions.py
@@ -55,3 +55,21 @@ class CategoricalCrossEntropy(Function):
     def derivative(y_true, y_pred):
         """Returns derivative of Categorical Cross Entropy for predicted and true values"""
         return -y_true / (y_pred + 10**-100)
+
+class SSE(Function):
+    """
+    Implementation of Sum of Squared Errors loss function extends Function class
+    """
+     
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def loss(y_pred, y_true):
+        """Returns Sum of Squareed errors for predicted and true values"""
+        return 0.5 * np.sum(np.power(y_true - y_pred, 2))
+
+    @staticmethod
+    def prime(y_true, y_pred):
+        """Returns derivative of Sum of Squared Errors for predicted and true values"""
+        return y_pred - y_true
